98623129a7bed4d36578d1b6219cb4f7e109b23d
Akshay <akshay.narayan@berkeley.edu>
2015-06-26 11:21:13 -0700

    added source-dest analysis

--- readFlowTrace.py
+++ readFlowTrace.py
@@ -17,12 +17,12 @@ def readFlows(filename):
         in [l.split() for l in open(filename)]
     ]
 
-def outputSimulatorFriendly(flows):
+def outputSimulatorFriendly(fname, flows):
     flows.sort(key = lambda x: x['time'])
-    template = "{} {} 0 0 {} 0 0 {} {}"
-    for f in flows:
-        #print f['size'], f['size']/1460, np.ceil(f['size']/1460)
-        print template.format(f['id'], "%.9f" % (f['time']/1e6 + 1.0), int(np.ceil(f['size']/1460)), f['src'], f['dst'])
+    template = "{} {} 0 0 {} 0 0 {} {}\n"
+    with open(fname, 'w') as out:
+        for f in flows:
+            out.write(template.format(f['id'], "%.9f" % (f['time']/1e6 + 1.0), int(np.ceil(f['size']/1460)), f['src'], f['dst']))
 
 def sliceByTime(flows):
     flows.sort(key = lambda x: x['time'])
@@ -36,18 +36,47 @@ def sliceByTime(flows):
         sliceStart = cutoff
     return flowsByTime
 
+def interarrival(flows):
+    return [flows[i+1]['time'] - flows[i]['time'] for i in range(len(flows)-1)]
+
+def sdAnalysis(flows):
+    hosts = set(f['src'] for f in flows)
+    sdpairs = sum(([(i,j) for j in hosts if i != j] for i in hosts), [])
+    sdflows = {(s,d):[f for f in flows if f['src'] == s and f['dst'] == d] for s,d in sdpairs}
+    sdstats = []
+    for sd in sdflows.keys():
+        fs = sdflows[sd]
+        fs.sort(key = lambda f:f['time'])
+        inter = interarrival(fs)
+        stats = (len(fs), np.median(inter), min(inter), max(inter)) if len(inter) > 0 else (0, 0,0,0,0)
+        sdstats.append((sd, stats))
+    sdstats.sort(key = lambda x:x[0][1])
+    sdstats.sort(key = lambda x:x[0][0])
+    print '(src, dst)', '(num flows, median interarrival, min inter, max inter)'
+    for t in sdstats:
+        print t[0], t[1]
+
+def burstinessAnalysis(flows):
+    lengths = [f['size'] for f in flows]
+    hist = np.histogram(lengths)
+    print 'Histogram'
+    print hist
+
+    byTime = sliceByTime(flows)
+    print 'by time'
+    print map(len, byTime)
+
 if __name__ == '__main__':
     if (len(sys.argv) < 2):
         print 'Usage: python readFlowTrace.py <flows.txt>'
         sys.exit(1)
-    flows = readFlows(sys.argv[1])
-    lengths = [f['size'] for f in flows]
-    hist = np.histogram(lengths)
-    #print 'Histogram'
-    #print hist
+    elif (len(sys.argv) > 2):
+        mode = sys.argv[1]
 
-    #byTime = sliceByTime(flows)
-    #print 'by time'
-    #print map(len, byTime)
+    flows = readFlows(sys.argv[-1])
 
-    outputSimulatorFriendly(flows)
+    sdAnalysis(flows)
+#    burstinessAnalysis(flows)
+
+    #outputSimulatorFriendly('sim_'+sys.argv[1], flows)
+
