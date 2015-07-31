24212de5aa48bee5de09c4526890d979ce217af6
Akshay <akshay.narayan@berkeley.edu>
2015-07-31 15:59:19 -0700

    fixed indentation

--- get_disk_io.py
+++ get_disk_io.py
@@ -10,20 +10,20 @@ while True:
     lineArr = re.sub(' +', ' ', line.strip()).split(' ')
     if "," in lineArr[0]:
         major = int(lineArr[0].split(",")[0])
-    minor = int(lineArr[0].split(",")[1])
-    cpu = int(lineArr[1])
-    seq = int(lineArr[2])
-    time = float(lineArr[3])
-    pid = int(lineArr[4])
-    action = lineArr[5]
-    rw = lineArr[6]
-    if len(lineArr) >= 11:
-        offset = lineArr[7]
-    assert(lineArr[8] == "+")
-    size = lineArr[9]
-    proc = lineArr[10:]
+        minor = int(lineArr[0].split(",")[1])
+        cpu = int(lineArr[1])
+        seq = int(lineArr[2])
+        time = float(lineArr[3])
+        pid = int(lineArr[4])
+        action = lineArr[5]
+        rw = lineArr[6]
+        if len(lineArr) >= 11:
+            offset = lineArr[7]
+            assert(lineArr[8] == "+")
+            size = lineArr[9]
+            proc = lineArr[10:]
 
-# action classifiers through IO event lifetime: Q G I D C
-# Q = queued, D = issue(d)/started, C = complete
-    if action == "D" and proc[0] == "[java]":
-        print "Time:", time, " Offset:", offset, " Size:", size, " Action:", action, " RW:", rw, " Proc:", proc
+            # action classifiers through IO event lifetime: Q G I D C
+            # Q = queued, D = issue(d)/started, C = complete
+            if action == "D" and proc[0] == "[java]":
+                print "Time:", time, " Offset:", offset, " Size:", size, " Action:", action, " RW:", rw, " Proc:", proc
