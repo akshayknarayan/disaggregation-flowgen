82a8bdd799b727928e622bfb9a44454ff3db9dab
Akshay <akshay.narayan@berkeley.edu>
2015-07-31 15:55:48 -0700

    removed collapsing flows, changed blkparse read from queue time to exec time

--- get_disk_io.py
+++ get_disk_io.py
@@ -4,12 +4,12 @@ import time
 import re
 
 while True:
-  line = sys.stdin.readline()
-  if line == "":
-    break
-  lineArr = re.sub(' +', ' ', line.strip()).split(' ')
-  if "," in lineArr[0]:
-    major = int(lineArr[0].split(",")[0])
+    line = sys.stdin.readline()
+    if line == "":
+        break
+    lineArr = re.sub(' +', ' ', line.strip()).split(' ')
+    if "," in lineArr[0]:
+        major = int(lineArr[0].split(",")[0])
     minor = int(lineArr[0].split(",")[1])
     cpu = int(lineArr[1])
     seq = int(lineArr[2])
@@ -18,9 +18,12 @@ while True:
     action = lineArr[5]
     rw = lineArr[6]
     if len(lineArr) >= 11:
-      offset = lineArr[7]
-      assert(lineArr[8] == "+")
-      size = lineArr[9]
-      proc = lineArr[10:]
-      if action == "Q" and proc[0] == "[java]":
+        offset = lineArr[7]
+    assert(lineArr[8] == "+")
+    size = lineArr[9]
+    proc = lineArr[10:]
+
+# action classifiers through IO event lifetime: Q G I D C
+# Q = queued, D = issue(d)/started, C = complete
+    if action == "D" and proc[0] == "[java]":
         print "Time:", time, " Offset:", offset, " Size:", size, " Action:", action, " RW:", rw, " Proc:", proc
