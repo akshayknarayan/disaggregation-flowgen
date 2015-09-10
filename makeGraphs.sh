b7ab9f77ffe20201182bb1a22d51af6f79195712
Akshay <akshay.narayan@berkeley.edu>
2015-09-10 01:24:52 -0700

    added nic graphs

--- makeGraphs.sh
+++ makeGraphs.sh
@@ -2,14 +2,11 @@
 
 cd results
 for dir in `ls`; do
+    if [ "$dir" == "Readme.md" ]; then continue; fi
     cd $dir
     echo $dir
-#    python ../../readFlowTrace.py rack-scale_plain rack-scale_plain_flows.txt
-#    python ../../readFlowTrace.py rack-scale_combined rack-scale_combined_flows.txt
-#    python ../../readFlowTrace.py rack-scale_timeonly rack-scale_timeonly_flows.txt
     python ../../readFlowTrace.py res-based_plain res-based_plain_flows.txt
-#    python ../../readFlowTrace.py res-based_combined res-based_combined_flows.txt
     python ../../readFlowTrace.py res-based_timeonly res-based_timeonly_flows.txt
-#    python ../../readFlowTrace.py nic nic_flows.txt
+    python ../../readFlowTrace.py nic nic_flows.txt
     cd ..
 done
